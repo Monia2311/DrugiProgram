191c578f2034e4bed0b6466fe946a2182c2b3388
Monia2311 <danychmonika@gmail.com>
2024-05-13 21:11:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Drugi_program_2 //własna przestrzeń nazw o nazwie Drugi_program_2 
+{
+    internal class Program  //klasa Program
+    {   //nad metodą (każdą metodą i klasą) widoczna jest liczba użyć wewnątrz kodu metody Main
+        static void Main(string[] args) //statyczna metoda Main
+        {
+            Console.WriteLine("Monika Danych");
+        }
+    }
+}
